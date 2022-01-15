e05f9f0bff6dc7efbece287b1d3f52f86279da25
FuxiaoLiu <60458715+FuxiaoLiu@users.noreply.github.com>
2022-01-15 11:42:18 -0500

    Update keyframe_extraction.py

--- preprocess/keyframe_extraction.py
+++ preprocess/keyframe_extraction.py
@@ -30,3 +30,40 @@ def save_i_keyframes(video_fn):
 if __name__ == '__main__':
     filename = './code/sample4.mp4'
     save_i_keyframes(filename)
+    
+    
+'''
+import os
+from concurrent import futures
+from tqdm import tqdm
+
+def extract_frames(video_name, out_folder, fps=2):
+    if os.path.exists(out_folder):
+        os.system('rm -rf ' + out_folder + '/*')
+        os.system('rm -rf ' + out_folder)
+    os.makedirs(out_folder)
+    cmd = 'ffmpeg -v 0 -i %s -r %d -q 0 %s/%s.jpg' % (video_name, fps, out_folder, '%08d')
+    os.system(cmd)
+
+
+target_path = '/fs/vulcan-projects/misc_forensics/DATA_FUXIAO/frame_and_transcript_sample/'
+files = os.listdir(target_path)
+
+for file in tqdm(files):
+    path1 = target_path + file
+
+    path2 = os.listdir(path1)
+
+    for p in path2:
+        if 'mp4' in p:
+            path3 = path1 + '/' + p
+
+    input_path = path3
+    output_path = path1 + '/frames_fps2'
+
+
+    if not os.path.exists(output_path):
+        os.mkdir(output_path)
+
+    extract_frames(input_path, output_path)
+'''
