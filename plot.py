5d94d23b1b2be660b58eff1643003ff6a2490ccd
FuxiaoLiu <60458715+FuxiaoLiu@users.noreply.github.com>
2022-04-06 21:05:38 -0400

    Update plot.py

--- plot.py
+++ plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 '''
 x = [4, 6, 8, 10,12,15,18,20,22]
 
-#点的横坐标
+
 k1 = [6, 10, 16, 22, 29, 37, 44, 53, 63, 69]
 k2 = [6, 13, 25, 28, 45, 48, 53, 69, 84, 93]
 
@@ -35,11 +35,11 @@ print(len(tmp_k1))
 print(len(tmp_k2))
 
 
-plt.plot(x,tmp_k1,'s-',color = 'r',label="Pyhop")#s-:方形
-plt.plot(x,tmp_k2,'o-',color = 'g',label="Metric-FF")#o-:圆形
-plt.xlabel("Number of Blocks")#横坐标名字
-plt.ylabel("Growth Rate of Plan Length")#纵坐标名字
-plt.legend(loc = "best")#图例
+plt.plot(x,tmp_k1,'s-',color = 'r',label="Pyhop")
+plt.plot(x,tmp_k2,'o-',color = 'g',label="Metric-FF")
+plt.xlabel("Number of Blocks")
+plt.ylabel("Growth Rate of Plan Length")
+plt.legend(loc = "best")
 plt.show()
 
 '''
@@ -74,9 +74,9 @@ for i in range(len(k2)):
 print(len(tmp_k1))
 print(len(tmp_k2))
 
-plt.plot(x,tmp_k1,'s-',color = 'r',label="Pyhop")#s-:方形
-plt.plot(x,tmp_k2,'o-',color = 'g',label="Metric-FF")#o-:圆形
-plt.xlabel("Number of Target")#横坐标名字
-plt.ylabel("Average Number of Plan Length")#纵坐标名字
-plt.legend(loc = "best")#图例
+plt.plot(x,tmp_k1,'s-',color = 'r',label="Pyhop")
+plt.plot(x,tmp_k2,'o-',color = 'g',label="Metric-FF")
+plt.xlabel("Number of Target")
+plt.ylabel("Average Number of Plan Length")
+plt.legend(loc = "best")
 plt.show()
